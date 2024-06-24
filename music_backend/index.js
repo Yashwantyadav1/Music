005162961c397a64497632f42bc592f2ce41b6cc
Yashraj Yadav <146312282+Yashraj78yadav@users.noreply.github.com>
2024-06-24 21:44:35 +0530

    passport-jwt setup and user setup

--- music_backend/index.js
+++ music_backend/index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');  // Import dotenv correctly
 dotenv.config();
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require("passport");
+const User = require("./models/user");
 // Initialize Express app
 const app = express();
 const port = process.env.PORT || 8000;
@@ -16,6 +20,27 @@ mongoose.connect(process.env.URI)
         console.error("Error connecting to MongoDB:", err.message);
     });
 
+    // passport-jwt setup
+
+
+let opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'thiskeysupposedToBeSecerate';
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        // done (error ,doestheuserExist)
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
+
 // Define a basic route
 app.get("/", (req, res) => {
     res.send("HELLO WORLD");
@@ -25,3 +50,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
 });
+
+
