ef2e67e3faed36a5b3d1f6ad61553790a088d8b9
Kundan-Soni <reachkundankumar@gmail.com>
2024-06-27 14:41:09 +0530

    updated login.js and passwordInput.js

--- music_frontend/src/routes/Login.js
+++ music_frontend/src/routes/Login.js
@@ -1,5 +1,6 @@
 
 import TextInput from "../components/shared/TestInput";
+import PasswordInput from "../components/shared/PasswordInput";
 const LoginComponent = () => {
     return (
         <div className = "w-full h-full flex flex-col items-center">
@@ -9,6 +10,7 @@ const LoginComponent = () => {
             <div className = "inputRegion w-1/3 py-10 flex items-center justify-center flex-col">
                 <div className="font-bold mb-12">To continue, login to Music.</div>
                 <TextInput label="Email address or username" placeholder="Email address or username"/>
+                <PasswordInput label="Password" placeholder="Password"/>
             </div>
         </div>
     )
