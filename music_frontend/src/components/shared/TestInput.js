385947f18f04d6d012f6eea61451cfea19dc0d5c
Kundan-Soni <reachkundankumar@gmail.com>
2024-06-27 14:21:19 +0530

    Update TestInput.js

--- music_frontend/src/components/shared/TestInput.js
+++ music_frontend/src/components/shared/TestInput.js
@@ -1,6 +1,6 @@
 const TextInput =() =>{
     return  (
-        <div className="testInputDiv flex flex-col space-y-2">
+        <div className="testInputDiv flex flex-col space-y-2 w-full">
             <label for="123" className="font-semibold"> Sample label </label>
             <input type="text" placeholder="Place" className="p-1 border border-gray-400 border-solid rounded placeholder-gray-500" id="123"/>
         </div>
