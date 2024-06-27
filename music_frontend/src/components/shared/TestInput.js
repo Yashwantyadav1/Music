981d22c2bc28565ddb53c7f65909f024b199f6c7
Kundan-Soni <reachkundankumar@gmail.com>
2024-06-27 14:03:15 +0530

    Update TestInput.js

--- music_frontend/src/components/shared/TestInput.js
+++ music_frontend/src/components/shared/TestInput.js
@@ -1,5 +1,10 @@
 const TextInput =() =>{
-    return  <input type="text" placeholder="Place" className="p-1 border border-gray-400 border-solid rounded placeholder-gray-500"/>
+    return  (
+        <div className="testInputDiv flex flex-col space-y-2">
+            <label> Sample label </label>
+            <input type="text" placeholder="Place" className="p-1 border border-gray-400 border-solid rounded placeholder-gray-500" id="123"/>
+        </div>
+    );
     
 };
 
